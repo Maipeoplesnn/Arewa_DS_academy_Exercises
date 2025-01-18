7a1697791b74b751e74c629a53b7e2da30510ae3
Abdullahi Maijamaa Yunusa <maipeople4008@gmail.com>
2025-01-18 17:56:35 +0100

    module 1

---
+++ day_1/Helloworld.py
@@ -0,0 +1,51 @@
+''' Exercise: Level 2
+1.Create a folder named day_1 inside 30DaysOfPython folder. 
+Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4.'''
+
+# Question 1.
+# python --version        # output: Python 3.12.4
+
+# Question 2.
+print(3 + 2)   # output: 5
+print(3 - 2)   # output: 1
+print(3 * 2)   # output: 6
+print(3 / 2)   # output: 1.5
+print(3 ** 2)  # output: 9
+print(3 % 2)   # output: 1
+print(3 // 2)  # output: 1
+
+# Question 3.
+print("Abdullahi Maijamaa Yunusa")
+print("Maijamaa")
+print("Nigeria")
+print("I am enjoying 30 days of python")
+
+# Question 4.
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Abdullahi Maijamaa Yunusa"))
+print(type("Maijamaa"))
+print(type("Nigeria"))
+
+
+
+
+
+# Exercise: Level 3
+# 1. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary
+print(type(100))                 
+print(type(15.7))                
+print(type(1 + 6j))              
+print(type('Asabeneh'))          
+print(type(10 == 100))           
+print(type([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(type((2.3, 31.2, 2))) 
+print(type({31.2, 2, 2.7, "hello world"})) 
+print(type({'name':'Asabeneh'})) 
+
+# Find an Euclidian distance between (2, 3) and (10, 8)
+eucl_d = (10 - 2)/(8 - 2)
+print(f"The Euclidean distance between (2, 3) and (10, 8) is {eucl_d}")
